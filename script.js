5d5691d22be88231f55d9f0f61a675fe9df37462
Jesus <jesuswrivas@gmail.com>
2022-12-12 11:14:50 -0500

    Console game version finished
    Create function gameRockPaperScissor.
    Create gameOn function.
    
    The gameOn function calls gameRockPaperScissor function on a for loop.
    Adding 1 to playerWins or computerWins, depending on who won that round.
    Whoever wins the most rounds is the ultimate winner.

--- script.js
+++ script.js
@@ -3,3 +3,48 @@ function getComputerChoice() {
     let randomOption = Math.floor(Math.random() * 3) + 1
     return choices[randomOption]
 }
+
+
+
+function gameRockPaperScissor(player, computer) {
+    //0 si es empate, 1 si ganó el jugador o 2 si ganó la computadora
+    let outcomes = { "rock": "scissor", "paper": "rock", "scissor": "paper" }
+    if (player === computer) {
+        return 0
+    } else if (outcomes[player] === computer) {
+        return 1
+    } else {
+        return 2
+    }
+
+}
+
+function gameOn() {
+    let playerWins = 0
+    let computerWins = 0
+    for (let i = 1; i < 6; i++) {
+        console.log(`Round ${i}`)
+        let player = prompt("What's your move?").toLowerCase()
+        let computer = getComputerChoice()
+        let result = gameRockPaperScissor(player, computer)
+        if (result === 1) {
+            console.log(`Player wins, ${player} beats ${computer}`)
+            playerWins++
+        } else if (result === 2) {
+            console.log(`Computer wins, ${computer} beats ${player}`)
+            computerWins++
+        } else if (result === 0) {
+            console.log("DRAW!")
+        }
+    }
+    if (playerWins === computerWins) {
+        return (`Its a draw! Player has won ${playerWins} times, and Computer ${computerWins} times as well!`)
+    } else if (playerWins > computerWins) {
+        return (`Player WINS! Player has won ${playerWins} times, and Computer ${computerWins} times!`)
+    } else {
+        return (`Computer WINS! Player has only won ${playerWins} times, and Computer ${computerWins} times!`)
+    }
+}
+
+console.log(gameOn())
+

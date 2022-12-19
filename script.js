e383dba9be5cb96c1f9975d971dbcc0a61a447f4
Jesus <jesuswrivas@gmail.com>
2022-12-19 12:42:26 -0500

    Logic on three buttons finished
    Create result board.
    Create the result styling.
    
    The game will reset once the player or the computer gets to 5

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
+
+
 let gameScore = { "player": 0, "computer": 0, "draw": 0 }
+let rockButtons = document.querySelectorAll(".container-buttons button")
+let scores = document.querySelectorAll(".container-results span")
+
+for (let button of rockButtons) {
+    button.addEventListener("click", (e) => {
+        /*Se genera la opción de la computadora */
+        let computerChoice = getComputerChoice()
+        /*Se inicia un juego */
+        let result = gameRockPaperScissor(e.target.dataset.choice, computerChoice)
+        /* Update Scorecard */
+        printResult(result)
+        winCheck(gameScore)
+
+    })
+}
 
 
-let rockButton = document.querySelector("#rock")
-rockButton.addEventListener("click", (e) => {
-    /*Se genera la opción de la computadora */
-    let computerChoice = getComputerChoice()
-    /*Se inicia un juego */
-    let result = gameRockPaperScissor(e.target.id, computerChoice)
-    /* Update Scorecard */
-    printResult(result)
-
-
-})
 
 /* FUNCTION DEFINITIONS 
-
+ 
 /* Returns a random option */
 function getComputerChoice() {
     let choices = { 1: "rock", 2: "paper", 3: "scissor" }
@@ -22,22 +28,25 @@ function getComputerChoice() {
     return choices[randomOption]
 }
 
-
-/*Plays a round it returns 0 if its a draw, 1 if player won and 0 if computer won */
+/*Plays a round it returns 0 if its a draw, 1 if player won and 2 if computer won */
 
 function gameRockPaperScissor(player, computer) {
+    h1Var = document.querySelector("h1")
 
     let outcomes = { "rock": "scissor", "paper": "rock", "scissor": "paper" }
     if (player === computer) {
+        h1Var.innerText = "Draw!"
         return 0
     } else if (outcomes[player] === computer) {
+        h1Var.innerText = "Player Wins this Round!"
         return 1
     } else {
+        h1Var.innerText = "Computer Wins this round!"
         return 2
     }
 
 }
-
+/*Updates the gameScore variable. The result input is the return value from the gameRockPaperScissor function*/
 function printResult(result) {
     if (result === 1) {
         ++gameScore.player
@@ -46,7 +55,36 @@ function printResult(result) {
     } else if (result === 0) {
         ++gameScore.draw
     }
+    scores[0].innerText = gameScore.player
+    scores[1].innerText = gameScore.computer
+    scores[2].innerText = gameScore.draw
+
 }
+
+
+/*Winner Check*/
+
+function winCheck({ player, computer, draw }) {
+    let h1Var = document.querySelector("h1")
+    if (player === 5) {
+        h1Var.innerText = "PLAYER WINS THE GAME!"
+        gameScore = { "player": 0, "computer": 0, "draw": 0 }
+        scores[0].innerText = 0
+        scores[1].innerText = 0
+        scores[2].innerText = 0
+        alert("A new round will start")
+
+    } else if (computer === 5) {
+        h1Var.innerText = "COMPUTER WINS THE GAME!"
+        gameScore = { "player": 0, "computer": 0, "draw": 0 }
+        scores[0].innerText = 0
+        scores[1].innerText = 0
+        scores[2].innerText = 0
+
+    }
+}
+
+
 
 
 

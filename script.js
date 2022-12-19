15d895f1bf88aa8a407596d3805d2c11e072d1fb
Jesus <jesuswrivas@gmail.com>
2022-12-19 15:54:19 -0500

    Finished rock,paper, scissor game
    Replace words with images (rock, paper, scissor buttons). An images
    was added as background.
    Improve scoring system. Now it will stay at the current score and stay there
    Create button at the bottom to reset game (reloads screen)
    Create a <p> element at the bottom that indicates the current player and computer move.
    At the beginning of the game it says: "Take your pick"

--- script.js
+++ script.js
@@ -6,16 +6,22 @@ let scores = document.querySelectorAll(".container-results span")
 
 for (let button of rockButtons) {
     button.addEventListener("click", (e) => {
-        /*Se genera la opción de la computadora */
-        let computerChoice = getComputerChoice()
-        /*Se inicia un juego */
-        let result = gameRockPaperScissor(e.target.dataset.choice, computerChoice)
-        /* Update Scorecard */
-        printResult(result)
-        winCheck(gameScore)
-
+        if ((gameScore.player === 5) || (gameScore.computer === 5)) {
+            return
+        } else {
+            /*The computer chooses its move*/
+            let computerChoice = getComputerChoice()
+            /*Initiates the game*/
+            let result = gameRockPaperScissor(e.target.dataset.choice, computerChoice)
+            /* Update Scorecard */
+            printResult(result)
+            winCheck(gameScore)
+        }
     })
 }
+
+let resetButton = document.querySelector(".resetbutton")
+resetButton.addEventListener("click", resetgame)
 
 
 
@@ -32,22 +38,27 @@ function getComputerChoice() {
 
 function gameRockPaperScissor(player, computer) {
     h1Var = document.querySelector("h1")
+    textVars = document.querySelector("p")
 
     let outcomes = { "rock": "scissor", "paper": "rock", "scissor": "paper" }
     if (player === computer) {
         h1Var.innerText = "Draw!"
+        textVars.innerText = `Its a DRAW!`
         return 0
     } else if (outcomes[player] === computer) {
         h1Var.innerText = "Player Wins this Round!"
+        textVars.innerText = `${player} beats ${computer}`
         return 1
     } else {
         h1Var.innerText = "Computer Wins this round!"
+        textVars.innerText = `${computer} beats ${player}`
         return 2
     }
 
 }
 /*Updates the gameScore variable. The result input is the return value from the gameRockPaperScissor function*/
 function printResult(result) {
+
     if (result === 1) {
         ++gameScore.player
     } else if (result === 2) {
@@ -63,26 +74,23 @@ function printResult(result) {
 
 
 /*Winner Check*/
-
 function winCheck({ player, computer, draw }) {
     let h1Var = document.querySelector("h1")
     if (player === 5) {
         h1Var.innerText = "PLAYER WINS THE GAME!"
-        gameScore = { "player": 0, "computer": 0, "draw": 0 }
-        scores[0].innerText = 0
-        scores[1].innerText = 0
-        scores[2].innerText = 0
-        alert("A new round will start")
 
     } else if (computer === 5) {
         h1Var.innerText = "COMPUTER WINS THE GAME!"
-        gameScore = { "player": 0, "computer": 0, "draw": 0 }
-        scores[0].innerText = 0
-        scores[1].innerText = 0
-        scores[2].innerText = 0
-
     }
 }
+
+
+function resetgame() {
+    window.location.reload()
+
+}
+
+
 
 
 
